959ac76bd4208e865ea0a49e00d04436bd2505d5
Dilan Jordan Segovia <140860304+Dilan1234542@users.noreply.github.com>
2024-07-07 16:02:46 -0500

    Se a inplementado un demo de la pantalla principal ademas se a implementado una pantalla de respiracion

--- src/App.jsx
+++ src/App.jsx
@@ -1,35 +1,44 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
+import { useState } from 'react';
+import './App.css';
+import {
+  BrowserRouter as Router,
+  Routes,
+  Route,
+  Link
+} from "react-router-dom";
+import Layout from './componets/layout';
+import Home from './componets/Home';
+import Blogs from './componets/blogs';
+import Respiracion from './componets/tecRespiracion';
+import NoPage from './componets/nopage';
+import logo from './assets/logo_calm.jpg'
 
 function App() {
-  const [count, setCount] = useState(0)
+  const [count, setCount] = useState(0);
 
   return (
-    <>
-      <div>
-        <a href="https://vitejs.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
+    <Router>
+      <div className="container">
+        <div className="sidebar">
+          <img src={logo} alt="Logo" className="logo_calm" />
+          <h1>Proyecto calm</h1>
+          <Link to="/home">Go to Home</Link>
+          <Link to="/blogs">Go to Blogs</Link>
+          <Link to="/contact">Tecnicas de Respiracion</Link>
+        </div>
+        <div className="content">
+          <Routes>
+            <Route path="/" element={<Layout />} />
+            <Route path="/home" element={<Home />} />
+            <Route path="/blogs" element={<Blogs />} />
+            <Route path="/contact" element={<Respiracion />} />
+            <Route path="*" element={<NoPage />} />
+          </Routes>
+
+        </div>
       </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
-    </>
-  )
+    </Router>
+  );
 }
 
-export default App
+export default App;
